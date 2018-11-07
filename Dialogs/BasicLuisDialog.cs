ec79a3c838015566d1ed847053e73838f6d67007
VenkataSambasivaRao <sambakesanam@gmail.com>
2018-11-07 12:09:23 +0400

    .

--- Dialogs/BasicLuisDialog.cs
+++ Dialogs/BasicLuisDialog.cs
@@ -46,20 +46,20 @@ namespace Microsoft.Bot.Sample.LuisBot
                 string message = "Glad to talk to you. Welcome to iBot - your Virtual Wasl Property Consultant.";
                 await context.PostAsync(message);
 
-                var activity = context.Activity as Activity;
-                var reply = activity.CreateReply("I have colors in mind, but need your help to choose the best one.");
-                reply.Type = ActivityTypes.Message;
-                reply.TextFormat = TextFormatTypes.Plain;
+    //            var activity = context.Activity as Activity;
+    //            var reply = activity.CreateReply("I have colors in mind, but need your help to choose the best one.");
+    //            reply.Type = ActivityTypes.Message;
+    //            reply.TextFormat = TextFormatTypes.Plain;
 
-                reply.SuggestedActions = new SuggestedActions()
-                {
-                    Actions = new List<CardAction>()
-    {
-        new CardAction(){ Title = "Blue", Type=ActionTypes.ImBack, Value="Blue" },
-        new CardAction(){ Title = "Red", Type=ActionTypes.ImBack, Value="Red" },
-        new CardAction(){ Title = "Green", Type=ActionTypes.ImBack, Value="Green" }
-    }
-                };
+    //            reply.SuggestedActions = new SuggestedActions()
+    //            {
+    //                Actions = new List<CardAction>()
+    //{
+    //    new CardAction(){ Title = "Blue", Type=ActionTypes.ImBack, Value="Blue" },
+    //    new CardAction(){ Title = "Red", Type=ActionTypes.ImBack, Value="Red" },
+    //    new CardAction(){ Title = "Green", Type=ActionTypes.ImBack, Value="Green" }
+    //}
+    //            };
 
                 PromptDialog.Text(
                 context: context,
